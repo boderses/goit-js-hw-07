65c9df721a01b2fa31b50f7b44714cb714176392
Bohdan Ses <bohdan.ses@gmail.com>
2022-07-17 22:03:42 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+function createLinks(array) {
+    return array.reduce((acc, picture) => acc + `<a class="gallery__link" href="">
+    <img
+      class="gallery__image"
+      src="${picture.preview}"
+      data-source="${picture.original}"
+      alt="${picture.description}"
+    />
+  </a>` , '');
+}
+
+const collection = createLinks(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("afterbegin", collection);
+
+
+// function onShowGallery(event) {
+//     const originalPicture = event.target.dataset.source;
+    
+// }
+
+// gallery.addEventListener("click", onShowGallery);
+
+
+
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll('.gallery__image');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+         e.preventDefault();
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.dataset.source
+        while (lightbox.firstChild) {
+          lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    });
+});
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+
+    lightbox.classList.remove('active')
+});
